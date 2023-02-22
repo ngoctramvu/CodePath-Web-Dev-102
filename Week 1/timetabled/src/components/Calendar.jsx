ad967ec13dc02e27df2ab69c2cdd6e3ce5a2354a
Tram Vu <vungoctram0108@gmail.com>
2023-02-21 22:05:08 -0500

    lab 01

---
+++ Week 1/timetabled/src/components/Calendar.jsx
@@ -0,0 +1,137 @@
+import React from "react";
+import Event from "./Event";
+
+const Calendar = () => {
+  return (
+    <div className="Calendar">
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Sunday</th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td className="time">8 am</td>
+            <td></td>
+            <td></td>
+            <Event event="Starbucks ☕️" color="green" location="646 Michigan Ave"/>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td className="time">9 am</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td className="time">10 am</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td className="time">11 am</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <Event event="Yolk 🍳" color ="green" location="355 E Ohio St"/>
+          </tr>
+
+          <tr>
+            <td className="time">12 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <Event event="Subway 🚊" color ="pink" location="Grand Station"/>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td className="time">1 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td className="time">2 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <Event event="The Bean 🫘" color="blue" location="Millenium Park"/>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td className="time">3 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td className="time">4 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <Event event="Subway 🚊" color ="pink" location="Washington Station"/>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td className="time">5 pm</td>
+            <td></td>
+            <Event event="Fancy Dinner 🎩" color="green" location="Maple & Ash"/>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Calendar;
